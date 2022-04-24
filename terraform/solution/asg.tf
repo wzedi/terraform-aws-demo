38f14062aa0213b6443d93708b68891d4dffe35a
warrick <wzedi@cadreworks.com>
2022-04-24 13:13:57 +1000

    adds the asg config

---
+++ terraform/solution/asg.tf
@@ -0,0 +1,28 @@
+module "asg" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+
+  # Autoscaling group
+  name = "${var.project_name}-${var.environment}-asg"
+
+  min_size                  = var.asg_min_size
+  max_size                  = var.asg_max_size
+  desired_capacity          = 1
+  wait_for_capacity_timeout = 0
+  health_check_type         = "EC2"
+  vpc_zone_identifier       = module.vpc.private_subnets
+
+  # Launch template
+  launch_template_name            = "launchtemplate"
+  launch_template_description     = "Launch template"
+  update_default_version          = true
+  launch_template_use_name_prefix = false
+
+  image_id          = var.asg_image_id
+  instance_type     = var.asg_instance_type
+  enable_monitoring = true
+
+  tags = {
+    Environment = var.environment
+    Project     = var.project_name
+  }
+}
--- terraform/solution/variables.tf
+++ terraform/solution/variables.tf
@@ -13,3 +13,27 @@ variable "environment" {
   description = "The environment"
   type        = string
 }
+
+variable "asg_min_size" {
+  description = "The minimum size of the autoscaling group"
+  type         = number
+  default      = 1
+}
+
+variable "asg_max_size" {
+  description = "The maximum size of the autoscaling group"
+  type         = number
+  default      = 1
+}
+
+variable "asg_image_id" {
+  description = "The AMI ID for the ASG launch template"
+  type        = string
+  default     = "ami-0d6fb2916ee0ab9fe"
+}
+
+variable "asg_instance_type" {
+  description = "The instance type for the ASG"
+  type        = string
+  default     = "t3.micro"
+}
