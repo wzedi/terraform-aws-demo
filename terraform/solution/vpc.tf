318c73f009106cdf97488c52a29ba2fce4e07891
warrick <wzedi@cadreworks.com>
2022-04-24 18:55:24 +1000

    fixes vpc name

--- terraform/solution/vpc.tf
+++ terraform/solution/vpc.tf
@@ -5,7 +5,7 @@ data "aws_availability_zones" "available" {
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "${var.project_name}-$(var.environment}-vpc"
+  name = "${var.project_name}-${var.environment}-vpc"
   cidr = "10.0.0.0/16"
 
   azs             = data.aws_availability_zones.available.names
