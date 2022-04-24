0b79c89b9885c448b468253b6094f3d558d31a3f
warrick <wzedi@cadreworks.com>
2022-04-24 12:47:59 +1000

    switches to aws vpc module

--- terraform/solution/vpc.tf
+++ terraform/solution/vpc.tf
@@ -1,8 +1,22 @@
-resource "aws_vpc" "main" {
-  cidr_block       = "10.0.0.0/16"
-  instance_tenancy = "default"
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "${var.project_name}-$(var.environment}-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = data.aws_availability_zones.available.names
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
 
   tags = {
-    Name = var.project_name
+    Terraform   = "true"
+    Project     = var.project_name
+    Environment = var.environment
   }
 }
